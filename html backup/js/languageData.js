a93b12f2c7c4761c227899393890a3121778b570
arcaea <43876510+arcaea@users.noreply.github.com>
2023-05-18 10:26:30 +0800

    Add files via upload

---
+++ html backup/js/languageData.js
@@ -0,0 +1,257 @@
+/* All Language Data */
+
+
+/* Traditional Chinese - Taiwan */
+L_taiwan = new Array(); 
+//tesla_1
+L_taiwan[0] = "電"; 
+//navbar
+L_taiwan[1] = "回首頁"; 
+L_taiwan[2] = "課程"; 
+L_taiwan[3] = "報名"; 
+L_taiwan[4] = "線上購物"; 
+L_taiwan[5] = "上課花絮"; 
+L_taiwan[6] = "團隊夥伴"; 
+L_taiwan[7] = "開課資訊"; 
+L_taiwan[8] = "關於我們"; 
+L_taiwan[9] = "聯絡我們"; 
+//about us
+L_taiwan[10] = "天賦科學實驗室是一個由精通各大領域天賦異稟的學生或老師組成的一個團隊，這裡會提供許多的知識，有科學知識，有關於資訊的知識，也有不同的課程可以參加"; 
+L_taiwan[11] = "我們致力於發展新的教學模式，透過實作而不是唸課本的教學方式來讓孩子體驗真實的情況"; 
+L_taiwan[12] = "想了解更多嗎?，趕快進來逛逛!"; 
+//course header
+L_taiwan[13] = "會玩又會讀書，你才有資格說你是天才!"; 
+//main
+L_taiwan[14] = "這是一個充滿夢想的地方，快來跟我們一起做實驗吧"; 
+//課程類別
+L_taiwan[15] = "物理化學"; 
+L_taiwan[16] = "生物化學"; 
+L_taiwan[17] = "鑑識科學"; 
+L_taiwan[18] = "生物醫學"; 
+//air
+L_taiwan[19] = "氣體製造"; 
+L_taiwan[20] = "我們利用薊頭漏斗、抽濾瓶和矽膠管等器材，用排水集氣法收集氧氣，製作方法利用催化劑催化雙氧水分解產生氧氣收集，並利用線香檢測發現會復燃產生火焰，接著製造氫氣也是利用相同的設備，用金屬加入稀鹽酸產生氫氣，並用排水集氣法收集，利用打火機做測試，會有爆鳴聲&火焰。"; 
+//aqur
+L_taiwan[21] = "生態系"; 
+L_taiwan[22] = "生態系有多大，包含了哪些範圍"; 
+//blood
+L_taiwan[23] = "血液辨識"; 
+L_taiwan[24] = "是什麼樣的溶液，噴在血跡上就能夠發出螢光?"; 
+//boat
+L_taiwan[25] = "汪洋中的小船"; 
+L_taiwan[26] = "利用油水不互溶的原理，來製作汪洋中的小船吧~"; 
+//cell
+L_taiwan[27] = "觀察細胞"; 
+L_taiwan[28] = "觀察植物細胞以及其細胞核，取出洋蔥的表皮細胞，將其放於載玻片滴上亞甲藍液後，可由顯微鏡觀察到植物的細胞核，並觀察不同的標本。"; 
+L_taiwan[29] = "例如:人類血液中的紅血球、豬的神經元等等，也可以利用簡易可攜帶的顯微鏡，直接夾於手機鏡頭上，搭配高科技的電子產品來觀察生活中的物品。"; 
+//COOHegg
+L_taiwan[30] = "醋蛋"; 
+L_taiwan[31] = "先將蛋打至培養皿中，觀察各種構造及了解各部位功能，也教導學員如何判斷雞蛋新鮮與否；將蛋放入裝有冰醋酸的容器中，由於蛋殼的成份是碳酸鈣，而碳酸鈣遇酸會產生二氧化碳，那些附著在蛋殼上的小氣泡就是二氧化碳，蛋殼所含的碳酸鈣與醋酸會產生反應，所以蛋殼會消失不見，最後會發現蛋稍微脹大且只剩一層薄膜呈現透明狀，此時能清楚觀察到醋蛋去殼後的蛋膜，以及蛋膜、蛋白、蛋黃因醋而造成的變性。"; 
+//crystal
+L_taiwan[32] = "結晶"; 
+L_taiwan[33] = "此實驗我們除了利用柳酸和甲醇合成出冬青油，經過萃取後，得到結晶"; 
+L_taiwan[34] = "此外還利用明礬讓學員明白如何利用飽和溶液和過飽和溶液來結晶出漂亮的晶體。"; 
+//eco bottle
+L_taiwan[35] = "生態瓶"; 
+L_taiwan[36] = "我們將石頭、水草、魚放進生態瓶中，就像一個小地球般，有消費者(魚) 、 生產者(植物)，分解者(細菌)，以光為能量，提供整個生態系系統的能量。"; 
+//elesugar
+L_taiwan[37] = "初階醣類檢測"; 
+L_taiwan[38] = "利用本氏液測定水溶液內是否含有還原醣，我們利用不同的溶液來測試，有太白粉水溶液、砂糖水溶液、樹薯粉水溶液、食鹽水溶液、市售飲料、葡萄糖水溶液，觀察不同顏色的變化得知醣類的含量。為什麼藉由顏色變化可知水溶液內是否含有醣類?其原理是什麼呢?"; 
+//slime
+L_taiwan[39] = "史萊姆"; 
+L_taiwan[40] = "是甚麼神奇的水，可以讓黏黏的膠水變成ㄉㄨㄞㄉㄨㄞ的彈力球呢?";
+//soap
+L_taiwan[41] = "製作肥皂"; 
+L_taiwan[42] = "平常我們洗澡用的肥皂是怎麼做成的呢?\n\n我們利用椰子油、水還有氫氧化鈉以一定的比例配成，就可以做出來囉~";
+//leaf
+L_taiwan[43] = "葉脈書籤"; 
+L_taiwan[44] = "此實驗可以自己摘取葉子，利用加熱後的氫氧化鈉水溶液，加入葉子，使強鹼破壞葉肉，數分鐘後，將葉子取出放入水盤，輕輕刷去葉肉即可看到葉脈，將整個葉脈刷去後吸乾水分，放入書中壓平，即可完成葉脈書籤。";
+//handwash
+L_taiwan[45] = "乾洗手"; 
+L_taiwan[46] = "我們將75%酒精加入瓶中，之後加入凝膠，使其變得凝狀。\n\n透過課堂講解，讓孩子們明白為何用濃度為75%的酒精， 勝過用100%的酒精，70%的效果最好為什麼要用75%?";
+//N2
+L_taiwan[47] = "液態氮"; 
+L_taiwan[48] = "目前可知世界上最冷的液體可以做什麼呢?分子料理?治療??";
+//stone
+L_taiwan[49] = "石膏"; 
+L_taiwan[50] = "石膏的用途是什麼?固定受傷的部位?做成擴香石?到底石膏有什麼更多的用途呢?";
+//HNAOH
+L_taiwan[51] = "認識酸鹼"; 
+L_taiwan[52] = "利用紫高麗菜汁作為酸鹼指示劑，並配製鹽酸以及氫氧化鈉水溶液，讓學生在12孔的容器中依序到滴出不同顏色 ，調整出正確的顏色，最後再將鹽酸(強酸)更改成醋酸(弱酸)，請學生一樣在盤中調出正確的顏色， 並比較前後兩者的差別。";
+//rainbowball
+L_taiwan[53] = "七彩粉圓"; 
+L_taiwan[54] = "利用海藻酸鈉+氯化鈣會形成類似薄膜的海藻酸鈣的特性， 並利用顏料調色，形成美麗的小粉圓。";
+//tesla_2
+L_taiwan[55] = "靜電的成因為物體因為互相摩擦，使受摩擦表面累積不易流動的電荷，常見的靜電現象例如:冬天脫毛衣，乾燥天氣下車摸手把時，撕開保鮮膜等等...";
+
+/*===============================================*/
+/* English */
+L_english = new Array(); 
+//tesla_1
+L_english[0] = "electronic"; 
+//navbar
+L_english[1] = "home page"; 
+L_english[2] = "course"; 
+L_english[3] = "Sign up"; 
+L_english[4] = "online shopping"; 
+L_english[5] = "course photo"; 
+L_english[6] = "our team"; 
+L_english[7] = "class information"; 
+L_english[8] = "about us"; 
+L_english[9] = "contact us"; 
+//about us
+L_english[10] = "Talent laboratory is a group consist of  students and teachers who are master in all field.You will be offered a lot of knowledge, such us science and technology, there are also some classes you can attend."; 
+L_english[11] = "We devote to developing new teaching methods, through do it by yourself to let children experience the real situations instead of learning from books"; 
+L_english[12] = "Do you want to know more about it? Come in and see more quickly!"; 
+//course header
+L_english[13] = "You are eligible to say you are a genius if you are good at studying and having fun at the same time."; 
+//main
+L_english[14] = "Let's do some experiences together in the place where full of dreams."; 
+//課程類別
+L_english[15] = "Physical chemistry"; 
+L_english[16] = "Biochemistry"; 
+L_english[17] = "Forensic Science"; 
+L_english[18] = "Biomedicine"; 
+//air
+L_english[19] = "gas production"; 
+L_english[20] = "We use thistle funnel,Filter Erlenmeyer flask, silicone tubing,and use collection of gases over water to collect oxygen.And use Catalyst  to catalyze Hydrogen peroxide to produce and collect oxygen.Also use incense to detect and find it  will reignite and produce flame.Then we use the same equipment to produce hydrogen. Add dilute by metal hydrochloric acid to produce hydrogen, then collect it by the method called collection of gases over water. Test it by lighter,and you will hear the Popping sound."; 
+//aqur
+L_english[21] = "ecosystem"; 
+L_english[22] = "How big is the ecosystem,and what is the";
+//blood
+L_english[23] = "blood  identifying"; 
+L_english[24] = "What kind of liquor will send out fluorescent when it sprayed on blood stains?"; 
+//boat
+L_english[25] = "boat of the ocean"; 
+L_english[26] = "Let's make \"boat of the ocean\" by the principle that means water repels oil."; 
+//cell
+L_english[27] = "cell observation"; 
+L_english[28] = "Observe plant cells and their nuclei,take out the epidermal cells of the onion,then put it on the\nslide glass ,and you can observe the nuclei of plant by microscope after dripping Methyl blue, and then observe different specimens."; 
+L_english[29] = "For example , Red blood in human\"s blood and Pig's neuron. Also you can use the easy and portable microscope that can  be clipped to the phone lens ,with the high technology electronic products to observe the objects in our life."; 
+//COOHegg
+L_english[30] = "vinegar egg"; 
+L_english[31] = "First, put the eggs in Petri Dish,then observe and understand the various of the configuration and the functions of all  different parts.  Also ,teach students how to determine if the eggs are fresh or not. Put the eggs in the container with glacial acetic acid. Because eggshell contains Calcium carbonate, and Calcium carbonate will cause CO2,those bubbles attach on eggshell are CO2. Calcium carbonate and acetic acid of eggshell will cause Chemical reaction,so .you will find the eggs are bigger , the eggshell will disappearancee   and becomes transparent. Now you can observe the vinegar egg 's membrane without  eggshell,and the changes of  egg membrane , protein, yolk caused by vinegar."; 
+//crystal
+L_english[32] = "crystallization"; 
+L_english[33] = "We use Salicylic acid and Methanol to admix wintergreen oil, after extracting you will get the crystallization in this experience ."; 
+L_english[34] = "In addition, we will use alum to let student know how to use saturated solution  and supersaturated solution to crystallize beautiful crystals."; 
+//eco bottle
+L_english[35] = "Eco bottle"; 
+L_english[36] = "We put some stones Aquatic plants and fishes in the plant terrariums,just like a small Earth.There are consumers (fishes),producers (plants ) ,decomposer(bacterial),and see light as energy, and offer the energy to all of the  Ecosystem system."; 
+//elesugar
+L_english[37] = "Preliminary of carbohydrate testing"; 
+L_english[38] = "Use Benedict's solution to test if there are reducing sugar in  aqueous solution, we use different solution to test, such as aqueous solution of potato starch, sugar solution ,cassava starch solution, saline solution, beverages, glucose solution.Observing the change of color to know how much is the carbohydrate. Why can we know if there are sugar or not by the change of color? And what is the principle of this phenomenon?"; 
+//slime
+L_english[39] = "slime"; 
+L_english[40] = "What the magical water is, can let sticky glue change into bouncy tension ball?";
+//soap
+L_english[41] = "making soap"; 
+L_english[42] = "How is the soap that we use to wash be made?  We use coconut oil,water and sodium hydroxide in fixed proportion to make it.";
+//leaf
+L_english[43] = "leaf vien bookmark"; 
+L_english[44] = "In this experience, you can pick leaves by yourself ,then put leaves into sodium hydroxide solution that is heaed, in order to let  mesophyll destructed bystrong base.After a few minutes  ,take out and put leaves in  water tray, you can see vein after brushing mesophyll gently.Brush away all the vein and  sop up moisture, then put it in a book, the leaf vien bookmark will finish.";
+//handwash
+L_english[45] = "hand sanitizer"; 
+L_english[46] = "We put 75% alcohol by volume into a bottle,then add  gel  to let it become gelatinous. It will let children know why  75% alcohol by volume is better then  absolute alcohol , and why   75% alcohol by volume is the most effective but we choose  the 75% alcohol by volume  through explaining in class.";
+//N2
+L_english[47] = "liquid nitrogen"; 
+L_english[48] = "What is the function of the coldest liquid in the world? Molecular gastronomy or therapy? \nwhat can we do by using the coldest...";
+//stone
+L_english[49] = "gypsum"; 
+L_english[50] = "What is the function of gypsum? To fix Injured part or to make aroma stone? And what is the more functions about gypsum?";
+//HNAOH
+L_english[51] = "understand acid base"; 
+L_english[52] = "We use  red cabbage juice as acid-base indicator, with hydrochloric acid and sodium hydroxide solution.Let students  drop out different color in a container with 12 holes  in order, then  adjust the corect color.And change hydrochloric acid to acetic acid . Then ask a student to adjust the correct color in plate, and compare the difference between them";
+//rainbowball
+L_english[53] = "colorful tapioca ball"; 
+L_english[54] = "We use  red cabbage juice as acid-base indicator, with hydrochloric acid and sodium hydroxide solution.Let students  drop out different color in a container with 12 holes  in order, then  adjust the corect color.And change hydrochloric acid to acetic acid . Then ask a student to adjust the correct color in plate, and compare the difference between them";
+//tesla_2
+L_english[55] = "Statics is form by objects mutual friction, to let the surface that is rubbed to  accumulate the electric charge that is not easy to flow . There are some electrostatic phenomenon we often see, for example, taking off a sweater in winter , touching door handle in dry weather, tearing  plasticwrap..etc.";
+
+
+/*===============================================*/
+/* Japan */ 
+L_japan = new Array(); 
+//tesla_1
+L_japan[0] = "電子"; 
+//navbar
+L_japan[1] = "ホーム"; 
+L_japan[2] = "コース"; 
+L_japan[3] = "お申し込み"; 
+L_japan[4] = "オンラインショッピング"; 
+L_japan[5] = "クラスの寫真"; 
+L_japan[6] = "チームメンバー"; 
+L_japan[7] = "好コース案内"; 
+L_japan[8] = "クラスについて"; 
+L_japan[9] = "お問い合わせ"; 
+//about us
+L_japan[10] = "タレントサイエンス実験室は、さまざまな分野や才能に精通した学生や教師が結成したチームであり、科学知識、情報に関する知識など多くの知識を提供し、さまざまなコースも参加できます。"; 
+L_japan[11] = "ただ教科書を読むことではなく、実践を通して子どもたちに真実状況を体験してもらうための新しい教育モデルの開発に取り組んでいます。"; 
+L_japan[12] = "もっと知りたいですか。早く入ってきて！"; 
+//course header
+L_japan[13] = "遊びも勉強も上手のあなたは天才と呼べ！"; 
+//main
+L_japan[14] = "ここは夢いっぱいの所です。早く私たちと一緒に実験をしましょう。"; 
+//課程類別
+L_japan[15] = "物理化学"; 
+L_japan[16] = "生物化学"; 
+L_japan[17] = "鑑識化学"; 
+L_japan[18] = "生物医学"; 
+//air
+L_japan[19] = "気体作り"; 
+L_japan[20] = "安全ロート、吸引フィルターボトル、シリコンチューブなどを使用し、排水集気方式で酸素を集めます。製造方法は、過酸化水素の分解を触媒し酸素を集め、線香でテストすると炎が発生します。水素の製造も同じ装置を使います。金属を使用し、薄い塩酸を加え、排水集気方式で水素を集めます。ライターを使用しテストすると、弾けるような音と炎が発生します。"; 
+//aqur
+L_japan[21] = "生態系"; 
+L_japan[22] = "生態系の大きさと含まれる範囲"; 
+//blood
+L_japan[23] = "血液認識"; 
+L_japan[24] = "血痕にスプレーすると蛍光が発する溶液は何ですか。"; 
+//boat
+L_japan[25] = "広々としている海の中の小舟"; 
+L_japan[26] = "油と水の非混和性原理を使い、海での小舟を作りましょう〜"; 
+//cell
+L_japan[27] = "細胞観察"; 
+L_japan[28] = "植物細胞及びその細胞核を観察します。タマネギの表皮細胞を取り出し、スライドガラスに載せ、メチレンブルー溶液を滴下後、顕微鏡で植物の細胞核とさまざまな標本を観察することができます。"; 
+L_japan[29] = "例えば、人間血液中の赤血球や豚の神経細胞など、シンプルでポータブルな顕微鏡を使用し、携帯電話のレンズを直接付け、ハイテク電子製品を使用し日常生活中の物を観察することもできます。"; 
+//COOHegg
+L_japan[30] = "酢卵"; 
+L_japan[31] = "まず、シャーレに卵を入れ、構造を観察し、各部分の機能を理解し、卵が新鮮かどうかを判断する方法を生徒に教えます。氷酢酸を入れた容器に卵を入れます。卵殻は炭酸カルシウムでできており、酸にさらされると炭酸カルシウムが二酸化炭素を発生します。卵殻に付着した小さな気泡は二酸化炭素です。卵殻に含まれる炭酸カルシウムが酢酸と反応して卵殻が消え、やがて卵が少し大きくなり、透明な膜だけが残ります。このとき、酢卵が卵殻を取り除いた卵膜や、酢による卵膜、卵白、卵黄の変異性がはっきりと観察できます。"; 
+//crystal
+L_japan[32] = "結晶"; 
+L_japan[33] = "この実験では、サリチル酸とメタノールを使用し、ウィンターグリーンオイルを合成してから抽出後の結晶ができる他に"; 
+L_japan[34] = "さらにミョウバンを使い、生徒さんに飽和溶液と過飽和溶液を使用し、美しい結晶ができる方法を理解させます。"; 
+//eco bottle
+L_japan[35] = "生態観察瓶"; 
+L_japan[36] = "石、海藻、魚を生態観察瓶の中に入れ、まるで小さな地球のよう、消費者(魚)、生産者(植物)、分解者(細菌)があり、光がエネルギーで生態系全体にエネルギーを供給します。"; 
+//elesugar
+L_japan[37] = "初段階糖分テスト"; 
+L_japan[38] = "ベネジクト液を使い、水溶液の中に還元糖が含まれているかどうかを測定します。コーンスターチ水溶液、グラニュー糖水溶液、タピオカ粉末水溶液、塩水水溶液、市販の飲料、グルコース水溶液などの溶液でテストし、色変化の観察によりどんな種類の糖分の量が分かります。なぜ色の変化で水溶液の中に糖分が含まれているのかが分かりますか。その原理は何ですか。"; 
+//slime
+L_japan[39] = "スライム"; 
+L_japan[40] = "どんな魔法の水はべたべたの接着剤をプリプリのスーパーボールに変えることができますか。";
+//soap
+L_japan[41] = "石鹸作り"; 
+L_japan[42] = "私たちは普段、入浴に使用する石鹸をどのように作られるのですか。\nココナッツオイル、水、水酸化ナトリウムを一定の割合で使って作れます。";
+//leaf
+L_japan[43] = "リーフブックマーク"; 
+L_japan[44] = "この実験では、生徒たちに葉を選ぶ。水酸化ナトリウム溶液を加熱し葉を入れます。強アルカリによって葉肉を破壊するようさせました。数分後、葉は取り出し水トレーに置き、葉肉をやさしく取り除き、葉の脈が確認できます。葉の脈全体を取り除いてた後、水を吸収して本で平らにしてリーフブックマークが完成します。";
+//handwash
+L_japan[45] = "ハンドジェル"; 
+L_japan[46] = "まずは75％のアルコールをボトルに注ぎ、次にジェルを加えて凝縮させます。\n教室での説明を通して、75％のアルコールが100％のアルコールよりも優れている理由を子供たちに理解させます。また、70％の効果が最適ですが、なぜ75％を使用しますか。"; 
+//N2
+L_japan[47] = "液体窒素"; 
+L_japan[48] = "現在、世界で最も冷たい液体で何ができるでしょうか。分子ガストロノミー用？治療用？";
+//stone
+L_japan[49] = "石膏"; 
+L_japan[50] = "石膏の用途は何ですか。けがのところを固定しますか。アロマストーンを作りますか。石膏には他にどんな用途がありますか。";
+//HNAOH
+L_japan[51] = "酸性とアルカリ性を知る"; 
+L_japan[52] = "紫色のキャベツジュースを酸塩基指示薬として使用し、塩酸と水酸化ナトリウムの水溶液を準備して、生徒が培養皿で正しい色を調整できるようにします。最後は塩酸(強酸)から酢酸(弱酸)まで、正しい色を調整するように生徒に依頼し、前後の2つを比較します。";
+//rainbowball
+L_japan[53] = "カラフルタピオカ"; 
+L_japan[54] = "アルギン酸ナトリウム+塩化カルシウムで、薄いフィルムのようなアルギン酸カルシウムが形成する特性を利用し、さらに顔料を使って着色すると、美しい小さな粉末の円が形成されます。";
+//tesla_2
+L_japan[55] = "静電気は、物体同士が擦れ合い、摩擦面に流れにくい電荷が蓄積することで発生します。一般的な静電気現象は、冬にセーターを脱ぐ時や乾燥した天候で車から降り、ドアハンドルを触わる時やラップをはがす時などの場合に発生します。";
+
+
+/*===============================================*/
